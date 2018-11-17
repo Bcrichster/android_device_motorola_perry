5b4728998b3bc122c3df1cbd1257ae340a0b93c3
Bcrichster <11166051+Bcrichster@users.noreply.github.com>
2018-11-16 20:54:42 -0500

    added some more ROM.mk files

---
+++ perry/xenonhd.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, device/motorola/perry/full_perry.mk)
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := perry
+PRODUCT_NAME := xenonhd_perry
+PRODUCT_BRAND := motorola
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="motorola/perry/perry:7.1.1/NPNS26.118-22-1/1:user/release-keys" \
+    PRIVATE_BUILD_DESC="perry-user 7.1.1 NPNS26.118-22-1 1 release-keys" \
+    PRODUCT_MODEL="Moto E4" \
+    PRODUCT_NAME="perry" \
+    PRODUCT_MODEL="perry"
